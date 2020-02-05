070b8ac49abcc3bba9268347dbce5c0a88e499e1
nicolasrubin985 <60698011+nicolasrubin985@users.noreply.github.com>
2020-02-05 12:24:18 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,11 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+    var a; 
+    var b;
+    a= document.getElementById("elNombre").value;   
+
+    b = document.getElementById("laEdad").value; 
+    alert("usted se llama " + a +  " y tiene " + b + " años" );
 }
 
